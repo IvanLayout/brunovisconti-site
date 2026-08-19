e299bf622d0f09d9a4a619168dc7fc2cf72ef0e7
VlaDSerga <65920800+VlaDSerga@users.noreply.github.com>
2026-08-19 12:03:34 +0300

    footer start

--- js/functions.js
+++ js/functions.js
@@ -268,6 +268,29 @@ $(() => {
 			showMaskOnHover: false
 		})
 	})
+
+
+	// commit
+
+	$('.form__input-anim').each(function(){
+		let value = $(this).val()
+
+		if ( value != '' ) {
+			$(this).closest('.form__field').addClass('_full')
+		} else {
+			$(this).closest('.form__field').removeClass('_full')
+		}
+	})
+
+	$('.form__input-anim').change(function() {
+		let value = $(this).val()
+
+		if ( value != '' ) {
+			$(this).closest('.form__field').addClass('_full')
+		} else {
+			$(this).closest('.form__field').removeClass('_full')
+		}
+	})
 })
 
 
