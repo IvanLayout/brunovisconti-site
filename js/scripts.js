88ff4c0c42b5d05ed8ef25a5e404b5b5c5898d41
VlaDSerga <65920800+VlaDSerga@users.noreply.github.com>
2026-08-25 06:48:06 +0300

    about

--- js/scripts.js
+++ js/scripts.js
@@ -302,6 +302,52 @@ $(() => {
 		})
 	}
 
+	if ($('.collections__slider').length) {
+		new Swiper(".collections__slider", {
+			loop: false,
+			spaceBetween: 20,
+			slidesPerView: 1,
+			watchSlidesProgress: true,
+			watchOverflow: true,
+			preloadImages: false,
+			lazy: {
+				loadPrevNext: true,
+				elementClass: 'lazyload',
+				enabled: true,
+				loadedClass: 'loaded',
+				checkInView: true,
+				loadOnTransitionStart: true
+			},
+			navigation: {
+				nextEl: '.slider-button-next',
+				prevEl: '.slider-button-prev'
+			},
+			breakpoints: {
+				'320': {
+					spaceBetween: 15,
+					slidesPerView: 1,
+				},
+				'480': {
+					spaceBetween: 15,
+					slidesPerView: 2,
+				},
+				'768': {
+					spaceBetween: 15,
+					slidesPerView: 3
+				},
+				'1024': {
+					spaceBetween: 20,
+					slidesPerView: 5
+				}
+			},
+			on: {
+				init: function (swiper) {
+					$(swiper.el).find('.swiper-wrapper').wrap('<div class="swiper-overflow"></div>')
+				}
+			}
+		})
+	}
+
 	$('body').on('submit', '.form-ajax', function (e) {
 		e.preventDefault()
 
