13ee0da1ad64900be7d93684691633af60421cc1
IvanLayout <ivanpetrachenkov.web@gmail.com>
2026-08-29 18:47:55 +0200

    header mob adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,14 @@ $(() => {
 	$('body').on('click', '.mob-menu-btn', function (e) {
 		e.preventDefault()
 
-		$('.header__top').addClass('_show')
-		$('.header__small-right').addClass('_show')
-		$('body').addClass('_lock')
-		$('.overlay').addClass('_show')
+		if ( $(this).hasClass('_active') ) {
+
+		} else {
+			$('.header__top').addClass('_show')
+			$('.header__small-right').addClass('_show')
+			$('body').addClass('_lock')
+			$('.overlay').addClass('_show')
+		}
 	})
 
 	if ($('.inner-news__slider').length) {
