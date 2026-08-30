a64d9a4b5929ddcd490ac8613b034e004ab37244
VlaDSerga <65920800+VlaDSerga@users.noreply.github.com>
2026-08-30 21:14:01 +0300

    main response 1023

--- js/scripts.js
+++ js/scripts.js
@@ -139,8 +139,8 @@ $(() => {
 	if ($('.reviews__slider').length) {
 		new Swiper(".reviews__slider", {
 			loop: false,
-			spaceBetween: 20,
-			slidesPerView: 1,
+			spaceBetween: 15,
+			slidesPerView: 'auto',
 			watchSlidesProgress: true,
 			watchOverflow: true,
 			preloadImages: false,
@@ -159,15 +159,15 @@ $(() => {
 			breakpoints: {
 				'320': {
 					spaceBetween: 15,
-					slidesPerView: 1,
+					slidesPerView: 'auto'
 				},
 				'480': {
 					spaceBetween: 15,
-					slidesPerView: 2,
+					slidesPerView: 'auto'
 				},
 				'768': {
 					spaceBetween: 15,
-					slidesPerView: 3
+					slidesPerView: 'auto'
 				},
 				'1024': {
 					spaceBetween: 20,
